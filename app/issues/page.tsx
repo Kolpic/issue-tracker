3948454e227ec35f60a5019a8effc922d6c8028d
Kolpic <galincho112@gmail.com>
2025-07-02 14:32:06 +0300

    Style the active link in the navbar

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IssuesPage = () => {
+    return (
+      <div>
+        <h1>Issues Page</h1>
+        <p>This is the issues page.</p>
+      </div>
+    )
+  }
+  
+  export default IssuesPage
