48ebe45603ecc295516934195d492d1a2fc0287b
Kolpic <galincho112@gmail.com>
2025-07-08 10:37:23 +0300

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,10 +4,14 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames'
+import { useSession } from 'next-auth/react'
+import { Box } from '@radix-ui/themes'
+import { Spinner } from './components'
 
 const NavBar = () => {
     const currentPath = usePathname();
-    console.log('Current Path:', currentPath);
+    const { status, data: session } = useSession();
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues/list' }
@@ -17,16 +21,27 @@ const NavBar = () => {
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href="/"><AiFillBug/></Link>
             <ul className='flex space-x-6'>
-                {links.map(link => 
-                <Link 
-                key={link.href} 
+                {links.map(link =>
+                <li key={link.href}>
+                <Link  
                 className={classNames({
                     'text-zinc-900': link.href == currentPath,
                     'text-zinc-500': link.href !== currentPath,
                     'hover:text-zinc-800 transition-colors': true
                 })} 
-                href={link.href}>{link.label}</Link>)}
+                href={link.href}>{link.label}</Link></li>)}
             </ul>
+            <Box>
+               {status === 'authenticated' && (
+                <Link href="/api/auth/signout">Log out</Link>
+               )}
+               {status == 'unauthenticated' && (
+                <Link href="/api/auth/signin">Log in</Link>
+               )}
+                {status === 'loading' && (
+                    <Spinner />
+                )}
+            </Box>
         </nav>
     )
 }
---
+++ app/auth/Provider.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+import React, { PropsWithChildren } from 'react'
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
+}
+
+export default AuthProvider
